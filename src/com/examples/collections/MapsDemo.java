3f7ec05112940ae8b5d8e30ac62b3df002a4830f
Vamshi Singireddy <svamshikiran@gmail.com>
2023-12-13 22:40:06 +0530

    Initial commit

---
+++ src/com/examples/collections/MapsDemo.java
@@ -0,0 +1,16 @@
+package com.examples.collections;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapsDemo {
+    public static void main(String[] args) {
+
+        Map<Integer, Student> studentMap = new HashMap<>();
+        Map<Integer, Student> studentLinkedMap = new LinkedHashMap<>();
+        Map<Integer, Student> studentIndexedMap = new TreeMap<>();
+
+    }
+}
---
+++ src/com/examples/collections/Student.java
@@ -0,0 +1,38 @@
+package com.examples.collections;
+
+public class Student {
+
+    private int rollno;
+    private String name;
+    private double marks;
+
+    public Student(int rollno, String name, double marks) {
+        this.rollno = rollno;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+}
