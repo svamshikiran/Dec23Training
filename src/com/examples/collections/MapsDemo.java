c78e0a5236bd40dcedb7578b7e5f7a427d53fa63
Vamshi Singireddy <svamshikiran@gmail.com>
2023-12-14 23:54:02 +0530

    Initial commit

--- src/com/examples/collections/MapsDemo.java
+++ src/com/examples/collections/MapsDemo.java
@@ -1,16 +1,23 @@
 package com.examples.collections;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class MapsDemo {
     public static void main(String[] args) {
 
         Map<Integer, Student> studentMap = new HashMap<>();
         Map<Integer, Student> studentLinkedMap = new LinkedHashMap<>();
-        Map<Integer, Student> studentIndexedMap = new TreeMap<>();
+        Map<Integer, Student> studentTreeMap = new TreeMap<>();
+
+        studentTreeMap.put(1, new Student(12, "TESTONE", 93.8));
+        studentTreeMap.put(4, new Student(13, "TESTTWO", 99));
+        studentTreeMap.put(2, new Student(14, "TESTTHREE", 98.7));
+        studentTreeMap.put(3, new Student(15, "TESTFOUR", 92));
+
+        Set<Integer> keySet = studentTreeMap.keySet();
+
+        keySet.forEach(key-> System.out.println(studentTreeMap.get(key).getRollno()));
+
 
     }
 }
---
+++ src/com/examples/collections/SetsDemo.java
@@ -0,0 +1,34 @@
+package com.examples.collections;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetsDemo {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        set.add(12);
+        set.add(23);
+        set.add(19);
+        set.add(8);
+
+        set.forEach(ele -> System.out.println(ele));
+
+        Set<Integer> linkedSet = new LinkedHashSet<>();
+        linkedSet.add(12);
+        linkedSet.add(23);
+        linkedSet.add(19);
+        linkedSet.add(8);
+        System.out.println("-----------------");
+        linkedSet.forEach(ele -> System.out.println(ele));
+
+        Set<Integer> treeSet = new TreeSet<>();
+        treeSet.add(12);
+        treeSet.add(23);
+        treeSet.add(19);
+        treeSet.add(8);
+        System.out.println("-----------------");
+        treeSet.forEach(ele -> System.out.println(ele));
+    }
+}
